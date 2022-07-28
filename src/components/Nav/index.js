75dc295a097c06d51dd51a30ff9f6d11da11aa56
alu1868 <alu1868@gmail.com>
2022-07-27 20:36:23 -0500

    added nav bar componenet + document icon

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,1 +1,36 @@
 import React from "react";
+
+function Nav({ currentPage, handlePageChange }) {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="#home"
+                    onClick={() => handlePageChange("Home")}
+                    className={currentPage === "Home" ? "nav-link active" : "nav-link"}>
+                        Home</a>
+                </li>
+                <li>
+                    <a href="#AboutMe"
+                    onClick={() => handlePageChange("AboutMe")}
+                    className={currentPage === "AboutMe" ? "nav-link active" : "nav-link"}>
+                        About Me</a>
+                </li>
+                <li>
+                    <a href="#Work"
+                    onClick={() => handlePageChange("Work")}
+                    className={currentPage === "Work" ? "nav-link active" : "nav-link"}>
+                        Work</a>
+                </li>
+                <li>
+                    <a href="#contact"
+                    onClick={() => handlePageChange("Contact")}
+                    className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>
+                        Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav
