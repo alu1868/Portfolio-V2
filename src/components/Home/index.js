4d835d8038d66a095f1af12f724c1f3b9e343d8c
alu1868 <alu1868@gmail.com>
2022-07-31 22:35:45 -0500

    added strengths

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,64 @@
 import React from "react"
 
+// Import Icons
+
+
 function Home() {
     return (
-        <h1>This is the Home screen</h1>
+        <content>
+            {/* --- Banner --- */}
+            <div>
+
+            </div>
+            {/* --- END Banner --- */}
+            {/* --- Strengths --- */}
+            <section>
+                {/* --- Problem Solving --- */}
+                <card>
+                    <div>
+                        <h2>Problem Solving</h2>
+                        <cardBody>
+                            <p>With my completion of the Full Stack Coding Bootcamp from the University of Texas, I have been equipped with relevant coding skills and mindset to pursue an unknown ocean of knowledge.</p>
+                            <p>That means being ready for failure and learning from mistakes, nothing beats finding an issue and optimizing the solutions.</p>
+                        </cardBody>
+                    </div>
+                </card>
+
+                {/* --- Teamwork/Communication */}
+                <card>
+                    <div>
+                        <h2>Teamwork/Communication</h2>
+                        <cardBody>
+                            <p>Being a team player means being a good communicator. I’m always dependable and ready to brainstorm ideas with others. Collaboration is the key to success and contributing to something bigger than yourself. </p>
+                            <p>Being a good team member means resolving the problem, not the ego.</p>
+                        </cardBody>
+                    </div>
+                </card>
+
+                {/* Creativity/Critical Thinking */}
+                <card>
+                    <div>
+                        <h2>Creativity & Critical Thinking</h2>
+                        <cardBody>
+                            <p>It's important to apply yourself to the situation, no problem has strictly one answer and there’s always room to improve. Put your own stamp on things, give it a twist, think beyond your walls.</p>
+                            <p>As an amateur film maker, I derive my influences from across the globe and am always looking for inspiration from every angle. </p>
+                        </cardBody>
+                    </div>
+                </card>
+
+                {/* Attentive/Aware */}
+                <card>
+                    <div>
+                        <h2>Attentive & Aware</h2>
+                        <cardBody>
+                            <p>Sometimes you’re the nail and sometimes you’re the hammer. Nothing really puts you in your place like being humbled by a mentor.</p>
+                            <p>I’m always striving for self improvement, and looking for advice from outside my reach.</p>
+                        </cardBody>
+                    </div>
+                </card>
+            </section>
+            {/* --- END Strengths --- */}
+        </content>
     )
 }
 
